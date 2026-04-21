4789e433f2fd0674973d0c8d801dc0864f658a7c
mahmoud0ssama <mahmoud02505@gmail.com>
2026-04-21 18:36:48 +0200

    fix: revert incorrect commit and apply correct changes

---
+++ Database/Alterations.sql
@@ -0,0 +1,14 @@
+ALTER TABLE cdr DROP COLUMN service_id;
+ALTER TABLE cdr ADD COLUMN service_type service_type;
+
+CREATE TABLE One_time_fee (
+    id   SERIAL PRIMARY KEY,
+    name   TEXT,
+    price   NUMERIC(5, 2)
+);
+
+CREATE TABLE contract_one_time (
+    contract_id     INTEGER REFERENCES contract(id),
+    fee_id          INTEGER REFERENCES One_time_fee(id)
+);
+

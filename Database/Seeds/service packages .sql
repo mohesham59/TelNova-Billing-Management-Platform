21753467bb88209e2a873ba9f8a1c620bad65bde
mohesham59 <mohesham357@gmail.com>
2026-04-21 22:31:13 +0200

    data: add seed data for rate plans and service packages

---
+++ Database/Seeds/service packages .sql
@@ -0,0 +1,8 @@
+INSERT INTO service_package (id, name, type, amount, priority) 
+VALUES (1, 'Voice Service', 'voice', 1000, 1);
+
+INSERT INTO service_package (id, name, type, amount, priority) 
+VALUES (2, 'SMS Service', 'sms', 500, 1);
+
+INSERT INTO service_package (id, name, type, amount, priority) 
+VALUES (3, 'Data Service', 'data', 10240, 1);

2ee6dcccd5495760da7728bdd99ff28117503a14
mohesham59 <mohesham357@gmail.com>
2026-04-21 22:33:34 +0200

    refactor(database): remove deprecated rateplan and service package files

--- Database/service packages .sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO service_package (id, name, type, amount, priority) 
-VALUES (1, 'Voice Service', 'voice', 1000, 1);
-
-INSERT INTO service_package (id, name, type, amount, priority) 
-VALUES (2, 'SMS Service', 'sms', 500, 1);
-
-INSERT INTO service_package (id, name, type, amount, priority) 
-VALUES (3, 'Data Service', 'data', 10240, 1);
